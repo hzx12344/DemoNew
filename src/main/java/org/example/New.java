fe32f38756634c64ebf24967e405cb38f81713f3
hzx12344 <1992997035@qq.com>
2023-07-08 12:52:38 +0800

    1.测试新的

---
+++ src/main/java/org/example/New.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * @ClassName New
+ * @Description TODO
+ * @Author 1475
+ * @Date 2023/7/8 12:49
+ * @Version 1.0
+ */
+
+public class New {
+    public static void main(String[] args) {
+        System.out.println("sudsudus");
+    }
+}
