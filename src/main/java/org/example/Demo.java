a917d0a25684a2450393ed277a9c188373888231
hzx12344 <1992997035@qq.com>
2023-07-08 12:42:09 +0800

    1.测试

---
+++ src/main/java/org/example/Demo.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * @ClassName Demo
+ * @Description TODO
+ * @Author 1475
+ * @Date 2023/7/8 12:40
+ * @Version 1.0
+ */
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("sdkskdskdksk");
+    }
+}
---
+++ src/main/java/org/example/Ttttss.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * @ClassName Ttttss
+ * @Description TODO
+ * @Author 1475
+ * @Date 2023/7/8 12:40
+ * @Version 1.0
+ */
+
+public class Ttttss {
+    public static void main(String[] args) {
+        System.out.println("dsdsss");
+    }
+}
