88e50e34043940a2452f5ddee8a05ccbff09351e
hzx12344 <1992997035@qq.com>
2023-07-08 12:39:35 +0800

    1.测试

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author 1475
+ * @Date 2023/7/8 12:28
+ * @Version 1.0
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("777777");
+        System.out.println("dsdsds");
+    }
+}
